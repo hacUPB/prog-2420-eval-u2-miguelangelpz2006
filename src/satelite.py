39fe88f0309912ae70c322aecddbcc567a198010
miguelangelpz2006 <miguel.perea@upb.edu.co>
2024-08-29 09:33:36 -0500

    satelite1

--- src/satelite.py
+++ src/satelite.py
@@ -1,3 +1,21 @@
+# Variables de entrada: Altitud inicial. Coeficiente de arrastre. Altitud minima de seguridad.
+# Variables de salida: Altitud final. El número de órbitas completadas. Si reingresó o no.
+#PSEUDOCODIGO
+                                                                                  #inicio
+    #Definir variables
+        ##imprimir:"Ingrese (altitud_inicial)"
+        ##Leer altitud inicial
+        ##imprmir: "ingrese (coeficiente_de_arrastre)"
+        ##Leer coeficiente_de_arrastre
+        ##imprimir:"Ingrese (altitud minima de seguridad)"
+    #simulacion de desintegracion orbital
+
+        
+
+
+
+
+
 
 def main():
     #Tu código va aquí
