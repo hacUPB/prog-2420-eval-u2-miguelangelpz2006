f8d0b13ee9cd583a2677792902e94aa8cca80408
miguelangelpz2006 <miguel.perea@upb.edu.co>
2024-09-09 16:15:06 -0500

    commit 2

--- src/reservas.py
+++ src/reservas.py
@@ -1,4 +1,18 @@
 nombre = str(input("ingrese su nombre "))
-apellido = int(input("ingrese su apellido"))  
-print("hola"nombre)
+apellido = str(input("ingrese su apellido"))  
+while True:
+    try:
+        titulos = int(input("¿Se identifica usted como señor o señora? 1. para señor 2. para señora:"))
+
+        if titulos == 1:
+            titulo = "señor"
+            break
+        if titulos == 2: 
+            titulo = "señora"
+            break
+        else:
+            print("entrada no valida, intente nuevamente.")
+    except ValueError:
+        print("vuelve a intentarlo por favor. :)")
+print(" Bienvenido a goku airlines", titulo, apellido)
 edad = int(input("ingrese su edad"))
